057cc0b3864ab0ffa752d2532a17d329f4a6fb88
bauerindustries <104348398+bauerindustries@users.noreply.github.com>
2022-11-09 22:41:29 +0000

    Customer view orders

--- models/order.model.js
+++ models/order.model.js
@@ -19,10 +19,62 @@ class Order {
     this.id = orderId;
   }
 
-  async save() {
+  static transformOrderDocument(orderDoc) {
+    return new Order(
+      orderDoc.productData,
+      orderDoc.userData,
+      orderDoc.status,
+      orderDoc.date,
+      orderDoc._id
+    );
+  }
+
+  static transformOrderDocuments(orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
+  }
+
+  static async findAll() {
+    const orders = await db
+      .getDb()
+      .collection('orders')
+      .find()
+      // sort in descending order by id, which are timestamped
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    const orders = await db
+      .getDb()
+      .collection('orders')
+      .find({ 'userData._id': uid })
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findById(orderId) {
+    const order = await db
+      .getDb()
+      .collection('orders')
+      .findOne({ _id: new mongodb.ObjectId(orderId) });
+
+    return this.transformOrderDocument(order);
+  }
+
+  save() {
     if (this.id) {
-      //update
-    } else {      
+      const orderId = new mongodb.ObjectId(this.id);
+      return db
+        .getDb()
+        .collection('orders')
+        .updateOne({ _id: orderId }, { $set: { status: this.status } });
+    } else {
       const orderDocument = {
         userData: this.userData,
         productData: this.productData,
@@ -30,7 +82,6 @@ class Order {
         status: this.status,
       };
 
-      // not using async/await, as no further action required/can handle promise where Order.save() is called
       return db.getDb().collection('orders').insertOne(orderDocument);
     }
   }
