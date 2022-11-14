add5ae1eff65c4bd80cb24916e12772ebf725167
bauerindustries <104348398+bauerindustries@users.noreply.github.com>
2022-11-14 20:40:21 +0000

    db config bugfix

--- config/session.js
+++ config/session.js
@@ -4,8 +4,13 @@ const mongoDbStore = require('connect-mongodb-session');
 function createSessionStore() {
   const MongoDbStore = mongoDbStore(expressSession);
 
+  let dbUri = process.env.MONGODB_URL;
+  if (dbUri == null || dbUri == '') {
+    dbUri = 'mongodb://localhost:27017';
+  }
+
   const store = new MongoDbStore({
-    uri: 'mongodb://localhost:27017',
+    uri: dbUri,
     databaseName: 'online-shop',
     collection: 'sessions',
   });
